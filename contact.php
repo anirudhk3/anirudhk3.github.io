6ba1eed532009178e90731d174d4bb55d1bae137
Anirudh Kumar <anirudh.kumar0109@gmail.com>
2021-01-04 23:30:23 -0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,21 +1,43 @@
 <?php 
-//variable setting
-$name = $_REQUEST['name'];
-$email = $_REQUEST['Email'];
-$message = $_REQUEST['Message'];
+
+use PHPMailer\PHPMailer\PHPMailer;
+if(isset($_POST['name']) && isset($_POST['email'])){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$body = $_POST['body'];
+	
+	require_once "PHPMailer/PHPMailer.php";
+	require_once "PHPMailer/SMTP.php";
+	require_once "PHPMailer/Exception.php";
+	
+	$mail = new PHPMailer();
+	
+	$mail->isSMTP();
+	$mail->HOST = "smpt.gmail.com";
+	$mail->SMPTAuth = true;
+	$mail->UserName = "anirudh.kumar0109@gmail.com";
+	$mail->Password = 'Transistor123@';
+	$mail->Port = 465;
+	$mail->SMTPSecure = "ssl";
+	
+	$mail->isHTML(true);
+	$mail->setFrom($email, $name);
+	$mail->addAddress("anirudh.kumar0109@gmail.com");
+	$mail->Subject = ("$email($subject)");
+	$mail->Body = $body;
+	
+	if($mail->send()){
+			$status = "success";
+			Sresponse = "Email is sent!";
+	}
+	else
+	{
+		$status = "failed";
+		$response = "Something is wrong: <br>" . $mail->ErrorInfo;
+	}
+	exit(json_encode(array("status"=> $status, "response" => $response)));
+	
+}
  
-
-
-//check input fields
-if(empty($name) || empty($email) || empty($message))
-{
-	echo "Please fill all the fields";
-}
-else
-{
-	mail("anirudh.kumar0109@gmail.com", "Anirudh Kumar Message", $message, "From: $name < $email>");
-	echo "<script type='text/javascript'>alert('your message sent successfully');
-	window.history.log(-1);
-	</script>";
-}
 ?>
