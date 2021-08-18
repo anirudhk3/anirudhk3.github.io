f36c9caea3ab6585c167638ff0a24097202543b3
Anirudh Kumar <anirudh.kumar0109@gmail.com>
2021-08-18 12:03:23 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,13 +5,15 @@ form.onsubmit = (e)=>{
   e.preventDefault(); //preventing form from submitting
   statusTxt.style.color = "#0D6EFD";
   statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
 
 let xhr = new XMLHttpRequest(); //creating new xml object
 xhr.open("POST", "message.php", true); //sending post request to message.php file
 xhr.onload = ()=> { //once ajax loaded
   if (xhr.readyState == 4 && xhr.status == 200) {// this means if no error
     let response = xhr.response; //storing ajax response
-    if(response.indexOf("Email and password field is required!") != -1 || response.indexOf("Enter a valid email address")) //if response is an error like enter valied email address change color to red
+    if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1) { //if response is an error like enter valied email address change color to red
     statusTxt.style.color = "red";
 }else { 
   form.reset();
@@ -20,7 +22,9 @@ xhr.onload = ()=> { //once ajax loaded
 },3000);
 }
   statusTxt.innerText = response;
+  form.classList.remove("disabled");
 
+}
 }
 let formData = new FormData(); //creating new FormData obj.
 xhr.send(formData); //sending form data
