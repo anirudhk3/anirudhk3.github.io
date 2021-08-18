8818045400a986164abab2a60166b5f4d1463575
Anirudh Kumar <anirudh.kumar0109@gmail.com>
2021-08-18 13:59:18 -0700

    Add files via upload

--- contact.php
+++ contact.php
@@ -12,6 +12,6 @@
   $headers = "From: $email_from \r\n";
   $headers .= "Reply-To: $visitor_email \r\n";
   mail($to,$email_subject,$email_body,$headers);
- header("Location: index.html");
+ header("Location: ContactMe.html");
 
 ?>
