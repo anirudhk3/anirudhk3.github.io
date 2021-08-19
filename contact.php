0774d70af11d992012f0fbe68d05d79bdca2439a
Anirudh Kumar <anirudh.kumar0109@gmail.com>
2021-08-18 19:11:15 -0700

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,17 +1,126 @@
 <?php
- $name = $_POST['name'];
- $visitor_email = $_POST['email'];
- $message = $_POST['message'];
+$postData = $uploadedFile = $statusMsg = '';
+$msgClass = 'errordiv';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    $postData = $_POST;
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    // Check whether submitted data is not empty
+    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
+        
+        // Validate email
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+        }else{
+            $uploadStatus = 1;
+            
+            // Upload attachment file
+            if(!empty($_FILES["attachment"]["name"])){
+                
+                // File path config
+                $targetDir = "uploads/";
+                $fileName = basename($_FILES["attachment"]["name"]);
+                $targetFilePath = $targetDir . $fileName;
+                $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+                
+                // Allow certain file formats
+                $allowTypes = array('pdf', 'doc', 'docx', 'jpg', 'png', 'jpeg');
+                if(in_array($fileType, $allowTypes)){
+                    // Upload file to the server
+                    if(move_uploaded_file($_FILES["attachment"]["tmp_name"], $targetFilePath)){
+                        $uploadedFile = $targetFilePath;
+                    }else{
+                        $uploadStatus = 0;
+                        $statusMsg = "Sorry, there was an error uploading your file.";
+                    }
+                }else{
+                    $uploadStatus = 0;
+                    $statusMsg = 'Sorry, only PDF, DOC, JPG, JPEG, & PNG files are allowed to upload.';
+                }
+            }
+            
+            if($uploadStatus == 1){
+                
+                // Recipient
+                $toEmail = 'kumarcricket901@gmail.com';
 
-  $email_from = 'anirudhk3@gmail.com';
-  $email_subject = "New Form Submission";
-  $email_body = "User Name: $name.\n". "User Email: $visitor_email.\n". "User Message: $message.\n";
+                // Sender
+                $from = 'sender@example.com';
+                $fromName = 'anirudhk3website';
+                
+                // Subject
+                $emailSubject = 'Contact Request Submitted by '.$name;
+                
+                // Message 
+                $htmlContent = '<h2>Contact Request Submitted</h2>
+                    <p><b>Name:</b> '.$name.'</p>
+                    <p><b>Email:</b> '.$email.'</p>
+                    <p><b>Subject:</b> '.$subject.'</p>
+                    <p><b>Message:</b><br/>'.$message.'</p>';
+                
+                // Header for sender info
+                $headers = "From: $fromName"." <".$from.">";
 
-  $to = "kumarcricket901@gmail.com";
-
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $visitor_email \r\n";
-  mail($to,$email_subject,$email_body,$headers);
- header("Location: ContactMe.html");
-
+                if(!empty($uploadedFile) && file_exists($uploadedFile)){
+                    
+                    // Boundary 
+                    $semi_rand = md5(time()); 
+                    $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+                    
+                    // Headers for attachment 
+                    $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+                    
+                    // Multipart boundary 
+                    $message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+                    "Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
+                    
+                    // Preparing attachment
+                    if(is_file($uploadedFile)){
+                        $message .= "--{$mime_boundary}\n";
+                        $fp =    @fopen($uploadedFile,"rb");
+                        $data =  @fread($fp,filesize($uploadedFile));
+                        @fclose($fp);
+                        $data = chunk_split(base64_encode($data));
+                        $message .= "Content-Type: application/octet-stream; name=\"".basename($uploadedFile)."\"\n" . 
+                        "Content-Description: ".basename($uploadedFile)."\n" .
+                        "Content-Disposition: attachment;\n" . " filename=\"".basename($uploadedFile)."\"; size=".filesize($uploadedFile).";\n" . 
+                        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+                    }
+                    
+                    $message .= "--{$mime_boundary}--";
+                    $returnpath = "-f" . $email;
+                    
+                    // Send email
+                    $mail = mail($toEmail, $emailSubject, $message, $headers, $returnpath);
+                    
+                    // Delete attachment file from the server
+                    @unlink($uploadedFile);
+                }else{
+                     // Set content-type header for sending HTML email
+                    $headers .= "\r\n". "MIME-Version: 1.0";
+                    $headers .= "\r\n". "Content-type:text/html;charset=UTF-8";
+                    
+                    // Send email
+                    $mail = mail($toEmail, $emailSubject, $htmlContent, $headers); 
+                }
+                
+                // If mail sent
+                if($mail){
+                    $statusMsg = 'Your contact request has been submitted successfully !';
+                    $msgClass = 'succdiv';
+                    
+                    $postData = '';
+                }else{
+                    $statusMsg = 'Your contact request submission failed, please try again.';
+                }
+            }
+        }
+    }else{
+        $statusMsg = 'Please fill all the fields.';
+    }
+}
 ?>
